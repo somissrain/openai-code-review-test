0b237e6da790837fa91fd7bc8d83027557a4a2a2
baizhi <15235385+silianruyuzhi@user.noreply.gitee.com>
2025-09-02 18:06:47 +0800

    代码评审（测试3）

--- test.java
+++ test.java
@@ -1,10 +1,9 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("白芷的代码评审测试1");
-        System.out.println("白芷的代码评审测试2");
-        System.out.println("白芷的代码评审测试3");
-        System.out.println("白芷的代码评审测试4");
-        System.out.println("白芷的代码评审测试5");
+
+        for(int i=0;i<5;i++){
+            System.out.println("白芷的代码评审测试"+i);;
+        }
     }
 }
